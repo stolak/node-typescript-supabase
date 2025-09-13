2be298d197024198ff57904945a2cc77983e434d
Akinbobola Olawole <stolaksoftech@yahoo.com>
2025-09-13 22:52:28 +0100

    Update swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,7 +10,7 @@ export const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: process.env.SWAGGER_SERVER_URL || "http://localhost:3000",
       },
     ],
     components: {
