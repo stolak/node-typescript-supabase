c4dae4427239edc633e5ad8e9033421583712028
Akinbobola Olawole <stolaksoftech@yahoo.com>
2025-11-13 09:42:01 +0100

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -66,6 +66,57 @@ router.post("/login", async (req: Request, res: Response) => {
     }
 
     const id = data.user.id;
+    const { data: userRolesData, error: userRolesError } = await supabase
+      .from("user_roles")
+      .select(
+        `
+        user_id,
+        role_code,
+        role:roles(
+          code,
+          name,
+          status,
+          privileges:role_privileges(
+            id,
+            description,
+            status,
+            created_at,
+            updated_at
+          ),
+          menus:role_menus(
+            id,
+            menu_id,
+            menu:menus(
+              id,
+              route,
+              caption
+            )
+          )
+         
+        )
+      `
+      )
+      .eq("user_id", String(id));
+    if (userRolesError) {
+      return res.status(500).json({ error: userRolesError.message });
+    }
+    let menus: Array<{ caption: string; route: string }> = [];
+    let privileges: Array<{ description: string; status: string }> = [];
+    if (userRolesData?.length > 0) {
+      const role = Array.isArray(userRolesData[0].role)
+        ? userRolesData[0].role[0]
+        : userRolesData[0].role;
+      menus =
+        role?.menus?.map((item: any) => ({
+          caption: item.menu?.caption,
+          route: item.menu?.route,
+        })) || [];
+      privileges =
+        role?.privileges?.map((item: any) => ({
+          description: item.description,
+          status: item.status,
+        })) || [];
+    }
 
     const { data: teacherData, error: teacherError } = await supabase
       .from("class_teachers")
@@ -76,11 +127,13 @@ router.post("/login", async (req: Request, res: Response) => {
       return res.json({
         ...data,
         teacher_id: null,
+        menus,
       });
     }
     res.json({
       ...data,
       teacher_id: teacherData?.id,
+      menus,
     });
   } catch (err) {
     console.log(err);
